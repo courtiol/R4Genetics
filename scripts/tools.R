c3e6a6da7a4c6edacf4cb33ea6ee8b67a1beb930
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-02-20 18:06:18 +0100

    Fix similarity computation, now identical to propShared()

--- scripts/tools.R
+++ scripts/tools.R
@@ -22,7 +22,7 @@ pairwise_F <- function(x, stat = "Fst", confint = TRUE, nboot = 100) {
   fmat <- matrix(nrow = npop, ncol = npop, dimnames = list(pops, pops))
   fmat_upr <- fmat_lwr <- fmat
   pb <- NULL
-  if (requireNamespace("progress", quietly = TRUE)) {
+  if (requireNamespace("progress", quietly = TRUE) & interactive()) {
     pb <- progress::progress_bar$new(total = npop*(npop - 1)/2)
   }
   if (!isNamespaceLoaded("progress")) {
@@ -44,42 +44,83 @@ pairwise_F <- function(x, stat = "Fst", confint = TRUE, nboot = 100) {
   return(fmat)
 }
 
-## TODO: FIX BUG? -> different from outcomes of propShared() but not sure why...
+
+# TODO: this is ugly and diploid only but I failed to find something smart...
+sharedness <- function(id1, id2) {
+  a <- id1[1]
+  b <- id1[2]
+  c <- id2[1]
+  d <- id2[2]
+  l <- length(unique(c(a, b, c, d)))
+  l1 <- length(unique(id1))
+  l2 <- length(unique(id2))
+  if (l == 1) return(2)
+  if (l == 2 & (l1 == 1 & l2 == 1)) return(0)
+  if (l == 2 & (l1 != 1 & l2 != 1)) return(2)
+  if (l == 2 & ((l1 == 1) | (l2 == 1))) return(1)
+  if (l == 3 & ((l1 == 1) | (l2 == 1))) return(0)
+  if (l == 3 & (l1 != 1 & l2 != 1)) return(1)
+  if (l == 4) return(0)
+  print(paste("id1 = ", id1, collapse = " "))
+  print(paste("id2 = ", id2, collapse = " "))
+  stop("case not accounted for in sharedness -> bug")
+  }
+
+
 similarity <- function(x, id1, id2) {
-  idA <- x@tab[id1, ]
-  idB <- x@tab[id2, ]
-  allelesA <- table(rep(names(idA), idA))
-  allelesA <- rep(names(allelesA), allelesA)
-  allelesB <- table(rep(names(idB), idB))
-  allelesB <- rep(names(allelesB), allelesB)
-  comparison <- do.call("rbind", lapply(locNames(x), function(locus) {
-    A <- unique(allelesA[grepl(locus, allelesA)])
-    B <- unique(allelesB[grepl(locus, allelesB)])
-    total <- length(unique(c(A, B)))
-    distinct <- length(c(setdiff(A, B), setdiff(B, A)))
-    common <- total - length(c(setdiff(A, B), setdiff(B, A)))
-    return(data.frame(locus, total, distinct, common))
-  }))
-  comparison_summary <- apply(comparison[, c("total", "distinct", "common")], 2, sum)
+  
+  idAB <- x@tab[c(id1, id2), ]
+  cols_with_NA <- apply(apply(idAB, 1, is.na), 1, any)
+  idAB <- idAB[, !cols_with_NA]
+  
+  alleles_count_to_df <- function(allele_counts_row) {
+    alleles <- table(rep(names(allele_counts_row), allele_counts_row))
+    alleles <- rep(names(alleles), alleles)
+    alleles_list <- strsplit(alleles, ".", fixed = TRUE)
+    data.frame(locus = unlist(lapply(alleles_list, function(x) x[[1]])),
+               allele = unlist(lapply(alleles_list, function(x) x[[2]])))
+  }
+
+  allelesA <- alleles_count_to_df(idAB[1, ])
+  allelesB <- alleles_count_to_df(idAB[2, ])
+  allelesAB <- data.frame(locus = allelesA$locus,
+                          idA =  allelesA$allele,
+                          idB = allelesB$allele)
+  
+  common <- sapply(by(allelesAB[, c("idA", "idB")],
+                                  INDICES = allelesAB$locus,
+                                  function(x) sharedness(x[, 1], x[, 2])),
+                   "c")
+  
+  allelesAB$common <- rep(common, 2) ## for debugging
+  
   comparison_summary <- data.frame(id1 = id1, id2 = id2,
-                                   distinct = comparison_summary["distinct"],
-                                   common = comparison_summary["common"],
-                                   total = comparison_summary["total"],
-                                   ratio = comparison_summary["common"]/comparison_summary["total"],
+                                   common = sum(common),
+                                   distinct = length(common) * 2 - sum(common),
+                                   total = length(common) * 2,
+                                   ratio = sum(common)/(length(common) * 2),
                                    stringsAsFactors = FALSE)
   comparison_summary
 }
+# data.frame(as.loci(myData[pop = 1, ]))[c(1,3), ]
+# similarity(myData, id1 = "AFBIBOR9503", id2 = "AFBIBOR9505")
+# propShared(myData[pop = 1])["AFBIBOR9503", "AFBIBOR9505"] 
 
-#similarity(myData, id1 = "N7", id2 = "N142")
-#propShared(myData[pop = 1])["N7", "N142"]
-#data.frame(as.loci(myData[pop = 1, ]))[c(1,3), ]
 
-##TODO: optimise and add pop
-pairwise_similarity <- function(x) {
+
+pairwise_similarity <- function(x, pop = NULL, as_vector = FALSE) {
   if ("loci" %in% class(x)) {
     x <- pegas::loci2genind(x)
   }
   if (!"genind" %in% class(x)) stop("x must be a genind object!")
+  
+  if (!is.null(pop)) x <- x[pop = pop]
+  
+  if (as_vector) {
+    ratio <- adegenet::propShared(x) # faster than using similarity()
+    return(ratio[upper.tri(ratio)])
+  }
+  
   indivs <- rownames(x@tab)
   nindivs <- length(indivs)
   if (nindivs < 2) stop("x needs to contain at least 2 individuals!")
@@ -92,7 +133,7 @@ pairwise_similarity <- function(x) {
                     ratio = rep(NA_real_, ncases),
                     stringsAsFactors = FALSE)
   pb <- NULL
-  if (requireNamespace("progress", quietly = TRUE)) {
+  if (requireNamespace("progress", quietly = TRUE) & interactive()) {
     pb <- progress::progress_bar$new(total = ncases)
   }
   if (!isNamespaceLoaded("progress")) {
@@ -112,3 +153,5 @@ pairwise_similarity <- function(x) {
 #test <- pairwise_similarity(myData[pop = 1])
 #res <- cbind(test, ratio2 = propShared(myData[pop = 1])[cbind(test[, 1], test[, 2])])
 #plot(res$ratio, res$ratio2)
+
+print("All functions succesfully loaded")
